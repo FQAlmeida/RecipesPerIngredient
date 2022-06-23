2d9e19703f40f05e19be06f3992dba6dd996d619
FQA <otavioalmeida650@protonmail.ch>
2022-06-23 16:46:32 -0300

    removed svelte

--- components/forms-svelte/src/NumberPicker.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    export let id: string;
-    export let label: string;
-    export let value: number;
-</script>
-
-<label for={id}>{label}:</label>
-<input type="number" {id} name={id} bind:value />
--- components/forms-svelte/src/TextBox.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    export let id: string;
-    export let label: string;
-    export let value: string;
-</script>
-
-<label for={id}>{label}:</label>
-<input type="text" {id} name={id} bind:value />
