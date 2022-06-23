c154a6f44e9595f01931a8bfa163f393e5e6eb31
FQA <otavioalmeida650@protonmail.ch>
2022-06-23 19:13:05 -0300

    schema update

--- apps/recipes-rest/src/persistence/getRecipes.ts
+++ apps/recipes-rest/src/persistence/getRecipes.ts
@@ -35,34 +35,35 @@ export async function getRecipes(args: Parameters<typeof getRecipesFromDB>) {
             cod: recipe_data.difficulty_level.cod_difficulty_level,
             difficult: recipe_data.difficulty_level.level
         },
+        tools: recipe_data.tools_recipe.map<ToolRegister>(tool_data => {
+            return {
+                cod: tool_data.tool.cod_tool,
+                is_required: tool_data.is_required,
+                name: tool_data.tool.name,
+                details: tool_data.tool.details
+            };
+        }),
+        ingredients: recipe_data.ingredients_recipe.map<IngredientRegister>(ingredient_data => {
+            return {
+                cod: ingredient_data.ingredient.cod_ingredient,
+                is_required: ingredient_data.is_required,
+                name: ingredient_data.ingredient.name,
+                measure_unit: {
+                    cod: ingredient_data.measurement_unit.cod_measurement_unit,
+                    name: ingredient_data.measurement_unit.name
+                },
+                quantity: ingredient_data.quatity
+            };
+        }),
+        cooking_time: recipe_data.cooking_time ? convertToDuration(recipe_data.cooking_time) : undefined,
+        preparation_time: convertToDuration(recipe_data.preparation_time),
         steps: recipe_data.steps.map<StepRegister>((data) => {
             return {
                 cod: data.cod_step,
                 depends_on: data.cod_depends_on_step || undefined,
                 description: data.description,
-                cooking_time: convertToDuration(data.cooking_time),
-                preparation_time: convertToDuration(data.preparation_time),
                 technique: getTechnique(data.technique),
-                tools: data.ToolsStep.map<ToolRegister>(tool_data => {
-                    return {
-                        cod: tool_data.tool.cod_tool,
-                        is_required: tool_data.is_required,
-                        name: tool_data.tool.name,
-                        details: tool_data.tool.details
-                    };
-                }),
-                ingredients: data.IngredientsStep.map<IngredientRegister>(ingredient_data => {
-                    return {
-                        cod: ingredient_data.ingredient.cod_ingredient,
-                        is_required: ingredient_data.is_required,
-                        name: ingredient_data.ingredient.name,
-                        measure_unit: {
-                            cod: ingredient_data.measurement_unit.cod_measurement_unit,
-                            name: ingredient_data.measurement_unit.name
-                        },
-                        quantity: ingredient_data.quatity
-                    };
-                })
+
             };
         })
     };
