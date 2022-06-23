ba0776b2ba22515cb16cfb799024966b7fc61d6a
FQA <otavioalmeida650@gmail.com>
2022-06-23 15:10:07 -0300

    begin filter form

---
+++ apps/recipes-web-interface-react/pages/recipes.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import type { NextPage } from "next";
+
+import { RandomRecipesFactory } from "recipe-models/src/factories/RandomRecipeFactory";
+import { RandomMediaFactory } from "recipe-models/src/factories/RandomMediaFactory";
+
+import { RecipeList } from "recipes-components-react/src/RecipesList";
+import { RecipeData } from "recipes-components-react/src/types/RecipeData";
+
+const Home: NextPage = () => {
+    const [recipes, set_recipes] = useState<RecipeData[]>([]);
+    const get_recipes = () => {
+        const recipes_data = RandomRecipesFactory(20);
+        return recipes_data.map((recipe_data) => {
+            const media = RandomMediaFactory(recipe_data.cod);
+            const recipe = Object.assign(recipe_data, { recipe_image_url: media.source });
+            return recipe;
+        });
+    };
+    useEffect(() => {
+        set_recipes(get_recipes());
+    }, []);
+    return (
+        <RecipeList recipes={recipes} />
+    );
+};
+
+export default Home;
