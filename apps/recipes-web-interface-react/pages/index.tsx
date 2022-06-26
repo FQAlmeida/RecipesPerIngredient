efb12fcb1a61a9e9b3d551fc0f098a18da1aa83e
FQA <otavioalmeida650@protonmail.ch>
2022-06-26 11:33:40 -0300

    add loading

--- apps/recipes-web-interface-react/pages/index.tsx
+++ apps/recipes-web-interface-react/pages/index.tsx
@@ -5,14 +5,20 @@ import { RandomRecipesFactory } from "recipe-models/src/factories/RandomRecipeFa
 import { RecipeRegister } from "recipe-models/src/models/Recipe";
 import { IngredientsFilterForm } from "recipes-components-react/src/RecipesFilter";
 import { RecipeList } from "recipes-components-react/src/RecipesList";
+import { Loading } from "recipes-components-react/src/Loading";
+
+type recipesState = (RecipeRegister & {
+    recipe_image_url: string;
+})[];
 
 const Home: NextPage = () => {
     const [ingredients, setIngredients] = useState<string[]>([]);
-    const [recipes, set_recipes] = useState<(RecipeRegister & {
-        recipe_image_url: string;
-    })[]>([]);
+    const [recipes, set_recipes] = useState<recipesState>([]);
+    const [filteredRecipes, setFilteredRecipes] = useState<recipesState>([]);
+    const [isLoading, setLoading] = useState(true);
 
     useEffect(() => {
+        setLoading(true);
         const get_recipes = () => {
             const recipes_data = RandomRecipesFactory(100);
             return recipes_data
@@ -24,23 +30,34 @@ const Home: NextPage = () => {
         };
         set_recipes(get_recipes());
     }, []);
-    const filterRecipes = () => {
-        if (!ingredients || ingredients.length <= 0) {
-            return recipes;
-        }
-        return recipes.filter((recipe_data) => {
-            for (const ingredientFilter of ingredients) {
-                if (recipe_data.ingredients.filter((ingredient) => ingredient.name.toLowerCase().includes(ingredientFilter.toLowerCase())).length > 0) {
-                    return true;
-                }
+
+    useEffect(() => {
+        const filterRecipes = () => {
+            if (!ingredients || ingredients.length <= 0) {
+                return recipes;
             }
-            return false;
-        });
-    };
+            return recipes.filter((recipe_data) => {
+                for (const ingredientFilter of ingredients) {
+                    if (recipe_data.ingredients.filter((ingredient) => ingredient.name.toLowerCase().includes(ingredientFilter.toLowerCase())).length > 0) {
+                        return true;
+                    }
+                }
+                return false;
+            });
+        };
+        setLoading(true);
+        setFilteredRecipes(filterRecipes());
+        setLoading(false);
+    }, [ingredients, recipes]);
+
+    if (isLoading) {
+        return <Loading />;
+    }
+
     return (
         <>
             <IngredientsFilterForm onIngredientsChange={(ingredients) => setIngredients(ingredients)} />
-            <RecipeList recipes={filterRecipes()} />
+            <RecipeList recipes={filteredRecipes} />
         </>
     );
 };
